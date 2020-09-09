7174df6f04528df0e7b1df55cbe419fd9e64c14f
inst94 <inst94@icloud.com>
2020-09-09 10:51:52 +0300

    Heroes

--- server.js
+++ server.js
@@ -2,9 +2,23 @@ let superheroes = require('superheroes');
 
 //console.log(superheroes.all;
 
-console.log(superheroes.random());
+/* console.log(superheroes.random());
 
 // цикл (покажи все элементы массива)
 superheroes.all.forEach(hero => {
     console.log(hero);
-});
+}); */
+
+let supervillains = require('supervillains');
+ 
+//console.log(supervillains.all);
+
+/* supervillains.all.forEach(supervillain => {
+    console.log(supervillain);
+}); */
+
+console.log(supervillains.random());
+
+console.log(`${superheroes.random()} fight ${supervillains.random()}`);
+
+console.log(superheroes.random(), 'fight with' , supervillains.random());
