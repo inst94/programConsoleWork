ab5a41ab06e74141d26468faae17bdaf5a081ec6
inst94 <inst94@icloud.com>
2020-09-09 09:52:54 +0300

    Project

---
+++ server.js
@@ -0,0 +1,10 @@
+let superheroes = require('superheroes');
+
+//console.log(superheroes.all;
+
+console.log(superheroes.random());
+
+// цикл (покажи все элементы массива)
+superheroes.all.forEach(hero => {
+    console.log(hero);
+});
